c9b7cc47632f8cafa88bc726f11ea767b363b16f
Елизавета Чеснокова <ILiza.Chesnokova@gmail.com>
2025-11-17 01:24:39 +0700

    Update set.h

--- set.h
+++ set.h
@@ -1,26 +1,60 @@
 #ifndef SET_H
 #define SET_H
+
 #include <string>
-#include <set>
+#include <iostream>
 
 using namespace std;
 
+// Структура узла для хеш-таблицы
+class SetNode {
+public:
+    string key;
+    int denseIndex;
+    SetNode* next;
+};
+
+
 class Set {
 private:
-    set<string> data;
     string name;
+    string* denseList;
+    SetNode** buckets;
+
+    int itemCount;
+    int capacityVal;
+    int bucketCount;
+
+    // Хеш-функция для строк
+    size_t calc_hash(const string& val) const;
+
+    // Метод расширения памяти
+    void resize_storage();
+
+    // Метод очистки памяти
+    void clear_memory();
 
 public:
     Set();
+
     Set(const string& setName);
 
+    Set(const Set& other);
+
+    Set& operator=(const Set& other);
+
+    ~Set();
+
     bool SADD(const string& value);
+
     bool SREM(const string& value);
+
     bool SISMEMBER(const string& value) const;
 
-    const set<string>& getData() const;
-    void setData(const set<string>& newData);
     const string& getName() const;
+
+    string* begin() const;
+    string* end() const;
 };
 
 #endif
