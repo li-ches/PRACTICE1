0af4d5f58cea5c983619088b660a11c0c83790d9
Елизавета Чеснокова <ILiza.Chesnokova@gmail.com>
2025-11-17 01:25:56 +0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,34 +1,87 @@
 #include "stack.h"
 
-Stack::Stack() : name("") {}
+// Конструктор по умолчанию
+Stack::Stack() : name(""), data(nullptr), capacity(0), size(0) {}
 
-Stack::Stack(const string& stackName) : name(stackName) {}
+// Конструктор с именем
+Stack::Stack(const string& stackName) : name(stackName), capacity(10), size(0) {
+    data = new string[capacity];
+}
 
+// Конструктор копирования
+Stack::Stack(const Stack& other) : name(other.name), capacity(other.capacity), size(other.size) {
+    data = new string[capacity];
+    for (int i = 0; i < size; ++i) {
+        data[i] = other.data[i];
+    }
+}
+
+// Оператор присваивания
+Stack& Stack::operator=(const Stack& other) {
+    if (this != &other) {
+        delete[] data;
+        name = other.name;
+        capacity = other.capacity;
+        size = other.size;
+        data = new string[capacity];
+        for (int i = 0; i < size; ++i) {
+            data[i] = other.data[i];
+        }
+    }
+    return *this;
+}
+
+// Деструктор
+Stack::~Stack() {
+    if (data != nullptr) {
+        delete[] data;
+    }
+}
+
+// Увеличение размера массива
+void Stack::resize() {
+    int newCapacity = (capacity == 0) ? 10 : capacity * 2;
+    string* newData = new string[newCapacity];
+    for (int i = 0; i < size; ++i) {
+        newData[i] = data[i];
+    }
+    delete[] data;
+    data = newData;
+    capacity = newCapacity;
+}
+
+// Добавление элемента
 bool Stack::SPUSH(const string& value) {
     if (value.empty()) return false;
-    data.push_back(value);
+    if (size == capacity) {
+        resize();
+    }
+    data[size++] = value;
     return true;
 }
 
+// Удаление и получение элемента
 string Stack::SPOP() {
-    if (data.empty()) return "ОШИБКА: Стек пуст";
-    string value = data.back();
-    data.pop_back();
-    return value;
+    if (size == 0) return "ОШИБКА: Стек пуст";
+    return data[--size];
 }
 
+// Проверка на пустоту
 bool Stack::isEmpty() const {
-    return data.empty();
+    return size == 0;
 }
 
-const vector<string>& Stack::getData() const {
+// Получение имени
+const string& Stack::getName() const {
+    return name;
+}
+
+// Итератор начала
+string* Stack::begin() const {
     return data;
 }
 
-void Stack::setData(const vector<string>& newData) {
-    data = newData;
-}
-
-const string& Stack::getName() const {
-    return name;
+// Итератор конца
+string* Stack::end() const {
+    return data + size;
 }
