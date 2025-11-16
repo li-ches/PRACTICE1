874146b3393d6e6c3bc10bd39a7a918dc9508134
Елизавета Чеснокова <ILiza.Chesnokova@gmail.com>
2025-11-17 01:23:29 +0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,27 +1,46 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <iostream>
 #include <string>
-#include <deque>
 
 using namespace std;
 
 class Queue {
 private:
-    deque<string> data;
     string name;
+    string* data;
+    int capacity;
+    int head;
+    int tail;
+
+    void reorganize_memory();
 
 public:
     Queue();
+
+    //Конструктор с именем очереди
     Queue(const string& queueName);
 
+    // Конструктор копирования
+    Queue(const Queue& other);
+
+    // Оператор присваивания
+    Queue& operator=(const Queue& other);
+
+    // Деструктор
+    ~Queue();
+
     bool QPUSH(const string& value);
+
     string QPOP();
+
     bool isEmpty() const;
 
-    const deque<string>& getData() const;
-    void setData(const deque<string>& newData);
     const string& getName() const;
+
+    string* begin() const;
+    string* end() const;
 };
 
 #endif
