2e7cbbd0ef1c3fec0ae7f237810978a5fb99ba09
Елизавета Чеснокова <ILiza.Chesnokova@gmail.com>
2025-11-17 01:16:10 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,19 +4,13 @@
 
 using namespace std;
 
+// Вывод справки по использованию
 void printUsage() {
-    cout << "Использование: ./dbms --file <имя_файла> --query '<запрос>'" << endl;
-    cout << "Доступные запросы:" << endl;
-    cout << "  SADD <имя_множества> <значение>" << endl;
-    cout << "  SREM <имя_множества> <значение>" << endl;
-    cout << "  SISMEMBER <имя_множества> <значение>" << endl;
-    cout << "  SPUSH <имя_стека> <значение>" << endl;
-    cout << "  SPOP <имя_стека>" << endl;
-    cout << "  QPUSH <имя_очереди> <значение>" << endl;
-    cout << "  QPOP <имя_очереди>" << endl;
-    cout << "  HSET <имя_хэш_таблицы> <ключ> <значение>" << endl;
-    cout << "  HDEL <имя_хэш_таблицы> <ключ>" << endl;
-    cout << "  HGET <имя_хэш_таблицы> <ключ>" << endl;
+    cout << "Использование: ./dbms --file <файл> --query '<запрос>'" << endl;
+    cout << "Примеры:" << endl;
+    cout << "  --query 'SADD myset value'" << endl;
+    cout << "  --query 'SPUSH mystack item'" << endl;
+    cout << "  --query 'HSET myhash key val'" << endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -27,35 +21,27 @@ int main(int argc, char* argv[]) {
         string arg = argv[i];
         if (arg == "--file" && i + 1 < argc) {
             filename = argv[++i];
-        } else if (arg == "--query" && i + 1 < argc) {
+        }
+        else if (arg == "--query" && i + 1 < argc) {
             query = argv[++i];
-        } else if (arg == "--help") {
+        }
+        else if (arg == "--help") {
             printUsage();
             return 0;
         }
     }
 
-    if (filename.empty()) {
-        cerr << "Ошибка: требуется аргумент --file" << endl;
+    if (filename.empty() || query.empty()) {
+        cout << "ОШИБКА: Не указаны обязательные параметры (--file и --query)." << endl;
         printUsage();
         return 1;
     }
 
-    if (query.empty()) {
-        cerr << "Ошибка: требуется аргумент --query" << endl;
-        printUsage();
-        return 1;
-    }
+    Database db(filename);
 
-    try {
-        Database db(filename);
-        string result = db.executeQuery(query);
-        cout << result << endl;
+    string result = db.executeQuery(query);
 
-    } catch (const exception& e) {
-        cerr << "Ошибка: " << e.what() << endl;
-        return 1;
-    }
+    cout << result << endl;
 
     return 0;
 }
